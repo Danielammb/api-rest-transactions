ba9fb7ed1a52b1c3ae7fdbe6e54d94e2a112a72b
Daniela Medeiros <45044558+Danielammb@users.noreply.github.com>
2024-12-10 16:45:09 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { env } from "./env";
 import { app } from "./app";
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen(env.PORT).then(() => {
   console.log(`Server listening `);
 });
